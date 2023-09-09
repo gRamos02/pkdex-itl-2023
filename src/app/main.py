1574df57bdbec7ea1413c46689b4a22c8779c9c3
Gerardo <gerardoere02@gmail.com>
2023-09-08 18:25:28 -0600

    Llamada a pkdex

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import requests
+from fastapi import FastAPI, HTTPException
+from typing import Dict
 
 app = FastAPI()
 
@@ -7,4 +9,29 @@ def root():
     print("hola")
     return {"Hello":"World"}
 
-# @app.get()
+
+def pokedex_call_by_id(id: int) -> Dict:
+    try:
+        url = f"https://pokeapi.co/api/v2/pokemon/{id}"
+        response = requests.get(
+            url=url,
+            timeout=30,
+        )
+        response.raise_for_status()
+        return response.json()  # response.json is a python dict
+    except requests.exceptions.HTTPError as err:
+        raise HTTPException(status_code=response.status_code, detail=f"{err}")
+    except requests.exceptions.SSLError as err:
+        raise HTTPException(status_code=response.status_code, detail=f"{err}")
+    except requests.exceptions.Timeout as err:
+        raise HTTPException(status_code=response.status_code, detail=f"{err}")
+    except requests.exceptions.RequestException as err:
+        raise HTTPException(status_code=response.status_code, detail=f"{err}")
+    except Exception as err:
+        raise HTTPException(status_code=response.status_code, detail=f"{err}") 
+
+
+@app.get("pokedex/{id}")
+async def pokedex_by_id(id: int) -> Dict:
+    return pokedex_call_by_id(id=id)
+    
